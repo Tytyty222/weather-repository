14d542e9c48e3cf62e32f233e3f405094b882fe2
Tytyty222 <lunri3965@gmail.com>
2026-05-16 18:05:58 +0300

    Create weather_types.py

---
+++ models/weather_types.py
@@ -0,0 +1,13 @@
+from models.weather_entry import WeatherEntry
+
+
+class SunnyWeather(WeatherEntry):
+    pass
+
+
+class RainyWeather(WeatherEntry):
+    pass
+
+
+class SnowyWeather(WeatherEntry):
+    pass
