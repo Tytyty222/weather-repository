ac252a69a2c6a80e599b392f66bb8d724f0909da
Tytyty222 <lunri3965@gmail.com>
2026-05-16 17:33:28 +0300

    Create weather_entry.py

---
+++ models/weather_entry.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+
+
+class WeatherEntry:
+
+    def __init__(
+        self,
+        date,
+        temperature,
+        description,
+        precipitation
+    ):
+
+        self.date = self.validate_date(date)
+        self.temperature = self.validate_temperature(
+            temperature
+        )
+
+        self.description = description
+        self.precipitation = precipitation
+
+    def validate_date(self, date_string):
+
+        try:
+            return datetime.strptime(
+                date_string,
+                "%Y-%m-%d"
+            )
+
+        except ValueError:
+            raise ValueError(
+                "Invalid date format. Use YYYY-MM-DD"
+            )
+
+    def validate_temperature(self, temperature):
+
+        try:
+            return float(temperature)
+
+        except ValueError:
+            raise ValueError(
+                "Temperature must be a number"
+            )
+
+    def to_dict(self):
+
+        return {
+            "date": self.date.strftime("%Y-%m-%d"),
+            "temperature": self.temperature,
+            "description": self.description,
+            "precipitation": self.precipitation
+        }
+
+    @classmethod
+    def from_dict(cls, data):
+
+        return cls(
+            data["date"],
+            data["temperature"],
+            data["description"],
+            data["precipitation"]
+        )
+
+    def __str__(self):
+
+        return (
+            f"Date: {self.date.strftime('%Y-%m-%d')}, "
+            f"Temperature: {self.temperature}°C, "
+            f"Description: {self.description}, "
+            f"Precipitation: {self.precipitation}"
+        )
